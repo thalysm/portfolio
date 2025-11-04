241e294ee59c65ce02770b0479cd3586b0f949aa
Thalys M <thalyscorreia@hotmail.com>
2025-11-03 22:46:01 -0300

    feat: melhorando para visualização mobile

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,23 +1,28 @@
 <script setup>
+import { ref } from 'vue'
 import PhotoMenu from '@/assets/images/PhotoMenu.png'
+
+const isMobileMenuOpen = ref(false)
 </script>
 
 <template>
   <header class="menu">
     <img :src="PhotoMenu" class="photo-menu" />
-    <div class="menu-items">
+
+    <button class="menu-toggle" @click="isMobileMenuOpen = !isMobileMenuOpen">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+
+    <div class="menu-items" :class="{ 'is-open': isMobileMenuOpen }">
       <p class="menu-item selected">Home</p>
       <p class="menu-item">Resume</p>
       <p class="menu-item">Projects</p>
       <p class="menu-item">Blog</p>
       <p class="menu-item">Contact</p>
     </div>
+
     <div class="hidden-photo"></div>
-    <!-- <div class="switch">
-      <div class="switch__1">
-        <input id="switch-1" type="checkbox" />
-        <label for="switch-1"></label>
-      </div>
-    </div> -->
   </header>
 </template>
