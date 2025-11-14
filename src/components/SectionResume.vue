99c731e9a52bcf131019522444ce96f700b3edb4
Thalys M <thalyscorreia@hotmail.com>
2025-11-14 19:28:33 -0300

    feat: melhorias na sessão resume

--- src/components/SectionResume.vue
+++ src/components/SectionResume.vue
@@ -32,20 +32,27 @@ const education = [
 <template>
   <section class="section-resume section-padding">
     <h2 class="section-title">Resume</h2>
-    <div class="resume-content">
-      <div class="resume-column">
-        <h3 class="resume-subtitle">Experience</h3>
-        <div v-for="exp in experiences" :key="exp.company" class="resume-item">
-          <h4>{{ exp.role }}</h4>
-          <h5>{{ exp.company }} | {{ exp.period }}</h5>
-          <p>{{ exp.description }}</p>
+    <div class="resume-timeline-wrapper">
+      <div class="timeline-group education-group">
+        <h3 class="resume-subtitle timeline-header">Education</h3>
+        <div v-for="edu in education" :key="edu.institution" class="timeline-item timeline-left">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content resume-item">
+            <h4>{{ edu.degree }}</h4>
+            <h5>{{ edu.institution }} | {{ edu.period }}</h5>
+          </div>
         </div>
       </div>
-      <div class="resume-column">
-        <h3 class="resume-subtitle">Education</h3>
-        <div v-for="edu in education" :key="edu.institution" class="resume-item">
-          <h4>{{ edu.degree }}</h4>
-          <h5>{{ edu.institution }} | {{ edu.period }}</h5>
+
+      <div class="timeline-group experience-group">
+        <h3 class="resume-subtitle timeline-header">Experience</h3>
+        <div v-for="exp in experiences" :key="exp.company" class="timeline-item timeline-right">
+          <div class="timeline-dot"></div>
+          <div class="timeline-content resume-item">
+            <h4>{{ exp.role }}</h4>
+            <h5>{{ exp.company }} | {{ exp.period }}</h5>
+            <p>{{ exp.description }}</p>
+          </div>
         </div>
       </div>
     </div>
