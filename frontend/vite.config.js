59aa91c0f634301becd7446fb51c1077905431d4
Thalys M <thalyscorreia@hotmail.com>
2025-11-16 21:42:59 -0300

    feat: fazendo proxy do container

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,5 +12,26 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  // --- ADICIONE ESTE BLOCO ---
+  // Isto funciona para 'npm run dev'
+  server: {
+    proxy: {
+      // Redireciona pedidos de /api para o backend
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
+  },
+  // E isto funciona para 'npm run preview' (que o Docker usa)
+  preview: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
+    }
   }
+  // --- FIM DO BLOCO ---
 })
