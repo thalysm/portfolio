7330a90675a667a005ba364b782c70f374ba53d4
Thalys M <thalyscorreia@hotmail.com>
2025-11-17 10:25:09 -0300

    feat: internacionalizando section resume e ligando com o backend

--- frontend/src/components/SectionResume.vue
+++ frontend/src/components/SectionResume.vue
@@ -1,74 +1,64 @@
 <script setup>
+import { ref, onMounted, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 import educationIcon from '@/assets/icons/Education.svg?url'
 import experienceIcon from '@/assets/icons/Experience.svg?url'
 
-defineProps({
-  currentStyle: String
+const { t, locale } = useI18n()
+const API_URL = ''
+const resumeItems = ref([])
+
+const getLoc = (obj) => obj ? (obj[locale.value] || obj.pt || obj) : ''
+
+onMounted(async () => {
+  try {
+    const res = await fetch(`${API_URL}/api/resume`)
+    if (res.ok) resumeItems.value = await res.json()
+  } catch (e) { console.error(e) }
 })
 
-const experiences = [
-  {
-    role: 'Front-End Developer',
-    company: 'Tech Solutions Inc.',
-    period: '2022 - Present',
-    description:
-      'Developed and maintained responsive web applications using Vue.js and React. Collaborated with UI/UX designers to implement modern interfaces.'
-  },
-  {
-    role: 'UI/UX Designer (Intern)',
-    company: 'DesignFlow Studio',
-    period: '2021 - 2022',
-    description:
-      'Assisted in creating wireframes, prototypes, and user flows for various mobile and web projects.'
-  }
-]
+const education = computed(() => resumeItems.value.filter(i => i.type === 'Education'))
+const experience = computed(() => resumeItems.value.filter(i => i.type === 'Experience'))
 
-const education = [
-  {
-    degree: 'Bachelor of Computer Engineering',
-    institution: 'Federal University of Uberaba',
-    period: '2017 - 2021'
-  }
-]
+const formatPeriod = (item) => {
+  return `${item.startDate} - ${item.isPresent ? (locale.value === 'pt' ? 'Presente' : 'Present') : item.endDate}`
+}
 </script>
 
 <template>
   <section class="section-resume section-padding">
-    <h2 class="section-title">Resume</h2>
+    <h2 class="section-title">{{ t('resume') }}</h2>
     <div class="resume-timeline-wrapper">
       <div class="timeline-group education-group">
         <div class="resume-subtitle timeline-header header-education">
-          <div class="header-content header-education">
-            <h3 class="title-text">Education</h3>
-            <div class="icon-circle">
-              <img :src="educationIcon" alt="Education Icon" />
-            </div>
+          <div class="header-content">
+            <h3 class="title-text">{{ t('education') }}</h3>
+            <div class="icon-circle"><img :src="educationIcon" /></div>
           </div>
         </div>
-        <div v-for="edu in education" :key="edu.institution" class="timeline-item timeline-left">
+        <div v-for="edu in education" :key="edu._id" class="timeline-item timeline-left">
           <div class="timeline-dot"></div>
           <div class="timeline-content resume-item">
-            <h4>{{ edu.degree }}</h4>
-            <h5>{{ edu.institution }} | {{ edu.period }}</h5>
+            <h4>{{ getLoc(edu.title) }}</h4>
+            <h5>{{ edu.institution }} | {{ formatPeriod(edu) }}</h5>
+            <p>{{ getLoc(edu.description) }}</p>
           </div>
         </div>
       </div>
 
       <div class="timeline-group experience-group">
         <div class="resume-subtitle timeline-header header-experience">
-          <div class="header-content header-experience">
-            <div class="icon-circle">
-              <img :src="experienceIcon" alt="Experience Icon" />
-            </div>
-            <h3 class="title-text">Experience</h3>
+          <div class="header-content">
+            <div class="icon-circle"><img :src="experienceIcon" /></div>
+            <h3 class="title-text">{{ t('experience') }}</h3>
           </div>
         </div>
-        <div v-for="exp in experiences" :key="exp.company" class="timeline-item timeline-right">
+        <div v-for="exp in experience" :key="exp._id" class="timeline-item timeline-right">
           <div class="timeline-dot"></div>
           <div class="timeline-content resume-item">
-            <h4>{{ exp.role }}</h4>
-            <h5>{{ exp.company }} | {{ exp.period }}</h5>
-            <p>{{ exp.description }}</p>
+            <h4>{{ getLoc(exp.title) }}</h4>
+            <h5>{{ exp.institution }} | {{ formatPeriod(exp) }}</h5>
+            <p>{{ getLoc(exp.description) }}</p>
           </div>
         </div>
       </div>
