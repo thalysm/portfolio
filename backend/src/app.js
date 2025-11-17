675bb46caab2110d3128d8c6495fb9e621169650
Thalys M <thalyscorreia@hotmail.com>
2025-11-17 10:16:14 -0300

    feat: adicionando rotas de categoria, curriculo

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,36 +3,34 @@ const cors = require('cors');
 const path = require('path');
 const dotenv = require('dotenv');
 
-// Carrega as variáveis de ambiente
 dotenv.config();
 
-// Importar rotas
 const authRoutes = require('./http/routes/authRoutes');
 const projectRoutes = require('./http/routes/projectRoutes');
 const uploadRoutes = require('./http/routes/uploadRoutes');
 const postRoutes = require('./http/routes/postRoutes');
 const contactRoutes = require('./http/routes/contactRoutes');
+const categoryRoutes = require('./http/routes/categoryRoutes');
+const resumeRoutes = require('./http/routes/resumeRoutes');
 
 const app = express();
 
-// Middlewares essenciais
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Servir arquivos estáticos (para uploads)
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
-// Rota de teste
 app.get('/', (req, res) => {
   res.send('API do Portfólio está funcionando!');
 });
 
-// Usar as rotas da API (prefixo /api)
 app.use('/api', authRoutes);
 app.use('/api', projectRoutes);
 app.use('/api', uploadRoutes);
 app.use('/api', postRoutes);
 app.use('/api', contactRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', resumeRoutes);
 
 module.exports = app;
